57d14101294fbd75eb3587f28826f4e1cf6d260f
Takuro Sesan <sesantakuro29@gmail.com>
2019-10-14 22:10:16 +0100

    Added Permutation and combination app

---
+++ PermutEtCombinos/PermutEtCombinos/FactorialFunctions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutEtCombinos
+{
+    class FactorialFunctions
+    {
+        public int factorials(int numero) {
+            int kicker = 1;
+           
+
+            if (numero == 0)
+            {
+                kicker = 1;
+            }
+
+            for (int i = 2; i <= numero; i++)
+            {
+                kicker *= i;
+            }
+
+            return kicker;
+        }
+
+        public int permutation(int numero, int raks)
+        {
+            
+            FactorialFunctions gaut = new FactorialFunctions();
+            int flopper = gaut.factorials(numero);
+            int floppee = gaut.factorials((numero-raks));
+
+            int permutation = flopper / floppee;
+
+            return permutation;
+        }
+
+        public int combinationNoRep(int numero, int raks)
+            {
+            FactorialFunctions gaut = new FactorialFunctions();
+            int flopper = gaut.factorials(numero);
+            int floppee = gaut.factorials(raks) * gaut.factorials( numero- raks);
+
+            int combinationNoRep = flopper / floppee;
+
+            return combinationNoRep;
+            }
+
+        public int combinationRep(int numero, int raks)
+            {
+            FactorialFunctions gaut = new FactorialFunctions();
+            int flopper = gaut.factorials(numero + raks - 1);
+            int floppee = gaut.factorials(raks) * gaut.factorials(numero - raks);
+
+            int combinationRep = flopper / floppee;
+
+            return combinationRep;
+            }
+
+    }
+}
---
+++ PermutEtCombinos/PermutEtCombinos/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutEtCombinos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.Write("Input your 'n' value: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                FactorialFunctions value = new FactorialFunctions();
+                int factorialOfNumber = value.factorials(numero);
+                //Console.WriteLine(factorialOfNumber);
+                Console.Write("Input your 'r' value: ");
+                int raks = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Console.WriteLine("What would you like to do today??(Permutation / Combination)");
+                string req = Console.ReadLine();
+                string request = req.ToLower();
+
+                if (request == "permutation")
+                {
+                    FactorialFunctions evaluation = new FactorialFunctions();
+                    int permutationValue = evaluation.permutation(numero, raks);
+                    Console.WriteLine(permutationValue);
+                }
+
+                else if (request == "combination")
+                {
+                    Console.WriteLine("Is repitition allowed???(Yes / No)");
+                    String reqRes = Console.ReadLine();
+                    String reqResL = reqRes.ToLower();
+                    if (reqResL == "yes")
+                    {
+                        FactorialFunctions evaluationist = new FactorialFunctions();
+                        int combinationRep = evaluationist.combinationRep(numero, raks);
+                        Console.WriteLine(combinationRep);
+
+                    }
+
+                    else if (reqResL == "no")
+                    {
+                        FactorialFunctions evaluationist = new FactorialFunctions();
+                        int combinationNoRep = evaluationist.combinationNoRep(numero, raks);
+                        Console.WriteLine(combinationNoRep);
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("I'm Sorry, I do not possess the ability to solve such functions,. Check back later and remember, Kapaichumarimarichupako");
+                }
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+
+
+    
+
